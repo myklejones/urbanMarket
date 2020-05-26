37ae1e87183c34930c8360d582a4dff1a3efbab6
mykle jones <mykle.j91@gmail.com>
2020-05-26 19:32:07 -0400

    convesation

---
+++ app/models/create_conversation.rb
@@ -0,0 +1,2 @@
+class CreateConversation < ApplicationRecord
+end
--- test/models/create_user_test.rb
+++ test/models/create_user_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserProfileTest < ActiveSupport::TestCase
+class CreateUserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
