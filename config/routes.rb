ae603bbe636ff09dff82b911a6f592a6382af13a
mykle jones <mykle.j91@gmail.com>
2020-06-20 16:17:46 -0400

    user & messages controller added

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 resources :users do
   resources :products
   resources :services
-  resources :conversations do 
-    resources messages
-  end
-  
+  resources :conversations  
+  resources :messages
 end
+
+resources :products, only:[ :index, :show]
+resources :services, only: [ :index, :show]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
