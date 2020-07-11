7d5ca7c5a5206b0801725831d34cd46e81a9523a
mykle jones <mykle.j91@gmail.com>
2020-07-11 17:19:52 -0400

    edit user migration "

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,15 @@
 class LoginController < ApplicationController
     def create
+        user = User.find_by(username: params[:username])
+  
 
-        
+        if user && user.authenticate(params[:password])
+          
+           $pass = params[:password]
+            render json: {token: create_token(user.id), user_id: user.id}
+            
+        else
+            render json: {errors: ["password or username does not match"]}, status: :unprocessable_entity
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_221701) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "converstions", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "recipient_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "markets", force: :cascade do |t|
-    t.string "category"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.boolean "read", default: false
-    t.bigint "user_id"
-    t.bigint "conversation_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.integer "quantity"
-    t.string "description"
-    t.integer "price"
-    t.bigint "market_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["market_id"], name: "index_products_on_market_id"
-  end
-
-  create_table "services", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.integer "price"
-    t.string "time_span"
-    t.bigint "user_id"
-    t.bigint "market_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["market_id"], name: "index_services_on_market_id"
-    t.index ["user_id"], name: "index_services_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
